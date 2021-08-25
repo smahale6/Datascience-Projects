c8625243bb7e7aa1f8b757aeed17070cfadbe049
Shrikanth <54486467+smahale6@users.noreply.github.com>
2021-08-25 10:33:03 -0400

    Insurance Fraud

---
+++ Insurance Fraud/activity_logger.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+
+class activity_logger:
+    def log(self,file_object,message):
+        now = datetime.now()
+        date = now.date()
+        current_time = now.strftime("%H:%M:%S")
+        file_object.write(str(date) + "/" + str(current_time) + "\t\t" + message +"\n")
